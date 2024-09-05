8b44bdfc34977a40315363094497f3873f16b653
Ranga Kulathunga <rangamadhusanka001@gmail.com>
2024-09-05 12:43:53 -0500

    change some register definitions of accumulator

--- src/mac3.v
+++ src/mac3.v
@@ -9,6 +9,9 @@ module mac3(
     
     reg [127:0] pixels, weights;
     reg [255:0] pr;
+    reg [135:0] s1r;
+    reg [71:0] s2r;
+    reg [37:0] s3r;
     wire [19:0] sum;
     
     wire [255:0] p;
@@ -20,6 +23,9 @@ module mac3(
         pixels <= pixelsIN;
         weights <= weightsIN;
         pr <= p;
+        s1r <= s1;
+        s2r <= s2;
+        s3r <= s3;
         sumOUT <= sum;
     end
     
@@ -42,15 +48,15 @@ module mac3(
     end
     endgenerate
     
-    adder_17 adder17_1(s1[135:119],s1[118:102],s2[71:54]);
-    adder_17 adder17_2(s1[101:85],s1[84:68],s2[53:36]);
-    adder_17 adder17_3(s1[67:51],s1[50:34],s2[35:18]);
-    adder_17 adder17_4(s1[33:17],s1[16:0],s2[17:0]);
+    adder_17 adder17_1(s1r[135:119],s1r[118:102],s2[71:54]);
+    adder_17 adder17_2(s1r[101:85],s1r[84:68],s2[53:36]);
+    adder_17 adder17_3(s1r[67:51],s1r[50:34],s2[35:18]);
+    adder_17 adder17_4(s1r[33:17],s1r[16:0],s2[17:0]);
     
-    adder_18 adder18_1(s2[71:54],s2[53:36],s3[37:19]);
-    adder_18 adder18_2(s2[35:18],s2[17:0],s3[18:0]);
+    adder_18 adder18_1(s2r[71:54],s2r[53:36],s3[37:19]);
+    adder_18 adder18_2(s2r[35:18],s2r[17:0],s3[18:0]);
     
-    adder_19 adder19_1(s3[37:19],s3[18:0],sum);
+    adder_19 adder19_1(s3r[37:19],s3r[18:0],sum);
     
 endmodule
 
